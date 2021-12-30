497ad652df001201dee599146fe9823cf05cc0b0
egileekada <egileoniso.ekada@gmail.com>
2021-12-30 22:18:04 +0100

    set default value

--- component/Screens/Home.tsx
+++ component/Screens/Home.tsx
@@ -16,6 +16,7 @@ export default function Home(props: any) {
     const [sucessModal, setSucessModal] = React.useState(false);
     const [transactionModal, setTransactionModal] = React.useState(false);
     const [tab, setTab] = React.useState(false)
+    const [transaction, setTransaction] = React.useState(false)
 
     React.useEffect(() => {
         {tab ? 
@@ -30,7 +31,16 @@ export default function Home(props: any) {
             <Header />
             <div className='w-full flex flex-col px-4 lg:px-8 ' >
                 <div className='w-full flex flex-col lg:flex-row mt-10'  >  
-                    <Transaction transac={setTransactionModal} open={setSavingModal} /> 
+                {transaction ?
+                        <Transaction transac={setTransactionModal} open={setSavingModal} /> 
+                    :
+                        <div className='w-full h-full flex justify-center items-center flex-col'>
+                            <svg className='my-6' width="48" height="34" viewBox="0 0 48 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M47.9944 16.7309H47.9992C47.9992 15.0644 47.3466 13.5503 46.2854 12.4209L46.2873 12.4191L37.0639 0.571991H10.5276L1.56997 12.6593L1.57381 12.6632C0.7264 13.6657 0.205249 14.9397 0.115424 16.33C0.048287 16.4604 0 16.6013 0 16.7584V32.4823C0 33.0053 0.423763 33.4281 0.945844 33.4281H47.0543C47.5773 33.4281 48.0001 33.0054 48.0001 32.4823V16.7584C48 16.749 47.9944 16.7404 47.9944 16.7309ZM11.3846 2.46368H36.2362L41.8665 10.434C41.8069 10.4321 41.7502 10.4245 41.6896 10.4245H30.3449V13.26C30.3449 16.4795 26.9645 15.7833 25.9281 15.7833H22.1505C21.1139 15.7833 17.7337 16.4795 17.7337 13.26V10.4245H6.38785C6.26964 10.4245 6.15421 10.4359 6.03693 10.4415L11.3846 2.46368ZM46.1084 31.5364H1.8925V16.7584C1.8925 16.749 1.88773 16.7405 1.8868 16.7309H1.97093C1.97093 14.2964 3.95232 12.316 6.38774 12.316H15.8419V13.2598C15.8419 16.5163 18.318 17.6748 22.1504 17.6748H25.9279C29.7612 17.6748 32.2364 16.5163 32.2364 13.2598V12.316H41.6915C42.9087 12.316 44.0125 12.8106 44.8126 13.6098L44.8145 13.6117C45.6137 14.4109 46.1074 15.5137 46.1074 16.7309H46.114C46.114 16.7404 46.1083 16.7488 46.1083 16.7584C46.1084 16.7584 46.1084 31.5364 46.1084 31.5364Z" fill="#192F5D"/>
+                            </svg> 
+                            <p className='font-Montserrat-Medium w-80 text-sm text-center' >You have no transaction.</p>
+                        </div>
+                }
 
                     <div className=' w-auto lg:w-700px lg:ml-8' >
                         <p style={{color: '#828282'}} className=' w-full font-Montserrat-ExtraBold text-sm mt-10' >Linked Cards</p>
--- component/dashboard-component/CreditCard.tsx
+++ component/dashboard-component/CreditCard.tsx
@@ -1,65 +1,76 @@
 import React from 'react'
 
 export default function CreditCard(props: any) {
+
+    const [show, setShow] = React.useState(false)
+
     return (
         <div className='lg:w-full w-auto dashboardslide flex overflow-x-auto flex-row mt-6 items-center' >
-            <div style={{ backgroundColor: '#6691FF'}} className=' w-auto h-48 ml-2 relative rounded-2xl p-6' > 
-                <div className=' w-60 flex ' >
-                    <div className='w-full flex flex-1' />
-                    <svg className='cursor-pointer' width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12.8835 6.31201C12.8835 6.31201 12.5215 10.802 12.3115 12.6933C12.2115 13.5967 11.6535 14.126 10.7395 14.1427C9.00016 14.174 7.25883 14.176 5.52019 14.1393C4.64085 14.1213 4.09219 13.5853 3.99419 12.698C3.78285 10.79 3.42285 6.31201 3.42285 6.31201" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M13.8053 4.15967H2.5" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M11.6274 4.15998C11.1041 4.15998 10.6534 3.78998 10.5507 3.27732L10.3887 2.46665C10.2887 2.09265 9.95006 1.83398 9.56406 1.83398H6.74206C6.35604 1.83398 6.01738 2.09265 5.91738 2.46665L5.75538 3.27732C5.65271 3.78998 5.20204 4.15998 4.67871 4.15998" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                </div> 
-                <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >Mba Bright Chisom</p>
-                <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >**** **** **** 9090</p>
-                <div className=' w-full mt-10 flex flex-row items-center ' > 
-                    <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >09/25</p>
-                    <div className='w-full flex flex-1' /> 
-                        <img src='/assets/images/mastercard.png' className='w-8' /> 
+            {show ? 
+                <>
+                    <div style={{ backgroundColor: '#6691FF'}} className=' w-auto h-48 ml-2 relative rounded-2xl p-6' > 
+                        <div className=' w-60 flex ' >
+                            <div className='w-full flex flex-1' />
+                            <svg className='cursor-pointer' width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12.8835 6.31201C12.8835 6.31201 12.5215 10.802 12.3115 12.6933C12.2115 13.5967 11.6535 14.126 10.7395 14.1427C9.00016 14.174 7.25883 14.176 5.52019 14.1393C4.64085 14.1213 4.09219 13.5853 3.99419 12.698C3.78285 10.79 3.42285 6.31201 3.42285 6.31201" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M13.8053 4.15967H2.5" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M11.6274 4.15998C11.1041 4.15998 10.6534 3.78998 10.5507 3.27732L10.3887 2.46665C10.2887 2.09265 9.95006 1.83398 9.56406 1.83398H6.74206C6.35604 1.83398 6.01738 2.09265 5.91738 2.46665L5.75538 3.27732C5.65271 3.78998 5.20204 4.15998 4.67871 4.15998" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </div> 
+                        <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >Mba Bright Chisom</p>
+                        <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >**** **** **** 9090</p>
+                        <div className=' w-full mt-10 flex flex-row items-center ' > 
+                            <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >09/25</p>
+                            <div className='w-full flex flex-1' /> 
+                                <img src='/assets/images/mastercard.png' className='w-8' /> 
+                        </div>
+                    </div>
+                    <div style={{ backgroundColor: '#FF9979' }} className=' lg:w-4 lg:h-40 w-auto h-48 ml-2 lg:ml-0 lg:p-0 relative rounded-2xl p-6 lg:rounded-tl-none lg:rounded-bl-none'>
+                        <div className='w-60 lg:hidden flex flex-col' >
+                            <div className='w-full flex ' >
+                                <div className='w-full flex flex-1' />
+                                <svg className='cursor-pointer' width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M12.8835 6.31201C12.8835 6.31201 12.5215 10.802 12.3115 12.6933C12.2115 13.5967 11.6535 14.126 10.7395 14.1427C9.00016 14.174 7.25883 14.176 5.52019 14.1393C4.64085 14.1213 4.09219 13.5853 3.99419 12.698C3.78285 10.79 3.42285 6.31201 3.42285 6.31201" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M13.8053 4.15967H2.5" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M11.6274 4.15998C11.1041 4.15998 10.6534 3.78998 10.5507 3.27732L10.3887 2.46665C10.2887 2.09265 9.95006 1.83398 9.56406 1.83398H6.74206C6.35604 1.83398 6.01738 2.09265 5.91738 2.46665L5.75538 3.27732C5.65271 3.78998 5.20204 4.15998 4.67871 4.15998" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                            </div> 
+                            <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >Mba Bright Chisom</p>
+                            <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >**** **** **** 9090</p>
+                            <div className=' w-full mt-10 flex flex-row items-center ' > 
+                                <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >09/25</p>
+                                <div className='w-full flex flex-1' /> 
+                                <img src='/assets/images/mastercard.png' className='w-8' /> 
+                            </div> 
+                        </div>
+                    </div>
+                    <div style={{ backgroundColor: '#FFE27C' }} className=' lg:w-4 lg:h-32 w-auto h-48 ml-2 lg:ml-0 lg:p-0 relative rounded-2xl p-6 lg:rounded-tl-none lg:rounded-bl-none'>
+                        <div className='w-60 lg:hidden flex flex-col' >
+                            <div className='w-full flex ' >
+                                <div className='w-full flex flex-1' />
+                                <svg className='cursor-pointer' width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M12.8835 6.31201C12.8835 6.31201 12.5215 10.802 12.3115 12.6933C12.2115 13.5967 11.6535 14.126 10.7395 14.1427C9.00016 14.174 7.25883 14.176 5.52019 14.1393C4.64085 14.1213 4.09219 13.5853 3.99419 12.698C3.78285 10.79 3.42285 6.31201 3.42285 6.31201" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M13.8053 4.15967H2.5" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M11.6274 4.15998C11.1041 4.15998 10.6534 3.78998 10.5507 3.27732L10.3887 2.46665C10.2887 2.09265 9.95006 1.83398 9.56406 1.83398H6.74206C6.35604 1.83398 6.01738 2.09265 5.91738 2.46665L5.75538 3.27732C5.65271 3.78998 5.20204 4.15998 4.67871 4.15998" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                            </div> 
+                            <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >Mba Bright Chisom</p>
+                            <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >**** **** **** 9090</p>
+                            <div className=' w-full mt-10 flex flex-row items-center ' > 
+                                <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >09/25</p>
+                                <div className='w-full flex flex-1' /> 
+                                <img src='/assets/images/mastercard.png' className='w-8' /> 
+                            </div> 
+                        </div>
+                    </div> 
+                </>
+            : 
+                <div className='w-full h-full flex justify-center items-center flex-col'> 
+                    <p className='font-Montserrat-Medium w-80 text-sm text-center' >You have no Debit Card.</p>
                 </div>
-            </div>
-            <div style={{ backgroundColor: '#FF9979' }} className=' lg:w-4 lg:h-40 w-auto h-48 ml-2 lg:ml-0 lg:p-0 relative rounded-2xl p-6 lg:rounded-tl-none lg:rounded-bl-none'>
-                <div className='w-60 lg:hidden flex flex-col' >
-                    <div className='w-full flex ' >
-                        <div className='w-full flex flex-1' />
-                        <svg className='cursor-pointer' width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12.8835 6.31201C12.8835 6.31201 12.5215 10.802 12.3115 12.6933C12.2115 13.5967 11.6535 14.126 10.7395 14.1427C9.00016 14.174 7.25883 14.176 5.52019 14.1393C4.64085 14.1213 4.09219 13.5853 3.99419 12.698C3.78285 10.79 3.42285 6.31201 3.42285 6.31201" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M13.8053 4.15967H2.5" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M11.6274 4.15998C11.1041 4.15998 10.6534 3.78998 10.5507 3.27732L10.3887 2.46665C10.2887 2.09265 9.95006 1.83398 9.56406 1.83398H6.74206C6.35604 1.83398 6.01738 2.09265 5.91738 2.46665L5.75538 3.27732C5.65271 3.78998 5.20204 4.15998 4.67871 4.15998" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                    </div> 
-                    <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >Mba Bright Chisom</p>
-                    <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >**** **** **** 9090</p>
-                    <div className=' w-full mt-10 flex flex-row items-center ' > 
-                        <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >09/25</p>
-                        <div className='w-full flex flex-1' /> 
-                        <img src='/assets/images/mastercard.png' className='w-8' /> 
-                    </div> 
-                </div>
-            </div>
-            <div style={{ backgroundColor: '#FFE27C' }} className=' lg:w-4 lg:h-32 w-auto h-48 ml-2 lg:ml-0 lg:p-0 relative rounded-2xl p-6 lg:rounded-tl-none lg:rounded-bl-none'>
-                <div className='w-60 lg:hidden flex flex-col' >
-                    <div className='w-full flex ' >
-                        <div className='w-full flex flex-1' />
-                        <svg className='cursor-pointer' width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12.8835 6.31201C12.8835 6.31201 12.5215 10.802 12.3115 12.6933C12.2115 13.5967 11.6535 14.126 10.7395 14.1427C9.00016 14.174 7.25883 14.176 5.52019 14.1393C4.64085 14.1213 4.09219 13.5853 3.99419 12.698C3.78285 10.79 3.42285 6.31201 3.42285 6.31201" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M13.8053 4.15967H2.5" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M11.6274 4.15998C11.1041 4.15998 10.6534 3.78998 10.5507 3.27732L10.3887 2.46665C10.2887 2.09265 9.95006 1.83398 9.56406 1.83398H6.74206C6.35604 1.83398 6.01738 2.09265 5.91738 2.46665L5.75538 3.27732C5.65271 3.78998 5.20204 4.15998 4.67871 4.15998" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                    </div> 
-                    <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >Mba Bright Chisom</p>
-                    <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >**** **** **** 9090</p>
-                    <div className=' w-full mt-10 flex flex-row items-center ' > 
-                        <p style={{color: 'white'}} className=' w-32 font-Montserrat-Medium text-sm' >09/25</p>
-                        <div className='w-full flex flex-1' /> 
-                        <img src='/assets/images/mastercard.png' className='w-8' /> 
-                    </div> 
-                </div>
-            </div> 
+            }
             <div className='lg:w-full flex lg:flex-1' />
-            <div onClick={()=> props.open(true)} className=' h-40 w-auto px-4 rounded-2xl ml-14 cursor-pointer mr-12 lg:mr-6 shadow-md flex justify-center items-center' >
+            <div onClick={()=> props.open(true)} className=' h-40 w-auto px-4  ml-14 rounded-2xl  cursor-pointer mr-12 lg:mr-6 shadow-md flex justify-center items-center' >
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="12" cy="12" r="11.5" fill="white" stroke="#CCD3D9"/>
                     <path d="M12 12L12 18M12 6V12V6ZM12 12L18 12L12 12ZM12 12H6H12Z" stroke="#CCD3D9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
